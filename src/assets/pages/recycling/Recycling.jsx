86d3fd8e7049fdf387254c538f1ba9b2309dc25a
Camilo Gs Univalle <jhojan.garcia@correounivalle.edu.co>
2024-10-27 06:52:16 -0500

    jcgs
    arreglo de header

--- src/assets/pages/recycling/Recycling.jsx
+++ src/assets/pages/recycling/Recycling.jsx
@@ -41,39 +41,6 @@ const RecyCling = () => {
   );
 };
 
-const Earth = () => {
-  return (
-    <>
-      {/* Renderiza el componente Header en la parte superior */}
-      <Header />
-      
-      {/* Contenedor para el modelo 3D con clase 'container-3d' */}
-      <div className="container-3d">
-        {/* Canvas que actúa como el área para renderizar el contenido 3D */}
-        <Canvas
-          camera={{
-            position: [0, 0, 10], // Posición inicial de la cámara para que el modelo se vea completo
-            fov: 70, // Campo de visión (Field of View) de la cámara, 70 grados
-          }}>
-          
-          {/* Controles para poder rotar el modelo 3D automáticamente */}
-          <OrbitControls
-            autoRotate={true} // El modelo rotará automáticamente
-          />
-          
-          {/* Luz ambiental que ilumina el modelo de manera uniforme */}
-          <ambientLight />
-          
-          {/* Luz direccional que ilumina desde una posición específica con una intensidad alta */}
-          <directionalLight position={[10, 10, 10]} intensity={5} />
-          
-          {/* Renderiza el modelo TrashCan, que contiene el modelo 3D */}
-          <Earth3d/>
-          <TrashCan />
-        </Canvas>
-      </div>
-    </>
-  );
-};
 
-export default RecyCling; Earth; // Exporta el componente RecyCling para ser usado en otras partes de la aplicación
+
+export default RecyCling; // Exporta el componente RecyCling para ser usado en otras partes de la aplicación
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,34 +9,28 @@ const Header = () => {
       <header>
         <nav>
           <ul>
-            {/* Cada <li> contiene un enlace a diferentes rutas usando el componente Link */}
             <li>
-              {/* Link a la página de inicio */}
               <Link to="/home">Inicio</Link>
             </li>
-            <li class="dropdown">
-              <a href="/map" class="dropbtn">Mapa</a>
-                <div class="dropdown-content">
-                    <a href="/soil-erosion">erosión</a>
-                    <a href="/management">residuos</a>
-                </div>
+            <li className="dropdown">
+              <Link to="/map" className="dropbtn">Mapa</Link>
+              <div className="dropdown-content">
+                <Link to="/soil-erosion">Erosión</Link>
+                <Link to="/management">Residuos</Link>
+              </div>
             </li>
             <li>
-              {/* Link a la página de reciclaje */}
               <Link to="/recycling">3D</Link>
             </li>
             <li>
-              {/* Link al quiz */}
-              <Link to="/quiz">quiz</Link>
+              <Link to="/quiz">Quiz</Link>
             </li>
-            <li class="dropdown">
-              {/* Link a la página de login */}
-              <a href="#profile" class="dropbtn">Perfil</a>
-                <div class="dropdown-content">
-                    <a href="/login">Mi perfil</a>
-                    <a href="#logout">Cerrar sesión</a>
-                </div>
-
+            <li className="dropdown">
+              <Link to="#profile" className="dropbtn">Perfil</Link>
+              <div className="dropdown-content">
+                <Link to="/login">Mi perfil</Link>
+                <Link to="#logout">Cerrar sesión</Link>
+              </div>
             </li>
           </ul>
         </nav>
