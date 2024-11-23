72dbffe4452789787a716db0eb6651cfdce4090d
Jonatan Medina <Jonatan.Medina@correounivalle.edu.co>
2024-11-23 18:04:18 -0500

    cambios de mayusculas en carpetad

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,12 @@ import "./index.css";
 import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
 
 import Login from './assets/pages/login/Login';
-import Home from './assets/pages/home/Introduction/Home';
+import Home from './assets/pages/home/introduction/Home';
 import Page404 from './assets/pages/page-404/Page404';
-import Deforestation from './assets/pages/home/content/Deforestation/deforestation';
+import Deforestation from './assets/pages/home/content/deforestation/deforestation';
 import Management from './assets/pages/home/content/improper-waste-management/management';
 import Biodiversity from './assets/pages/home/content/loss-of-biodiversity/biodiversity';
-import Map from './assets/pages/home/Map/map';
+import Map from './assets/pages/home/map/map';
 import We from './assets/pages/home/nosotros/we';
 import Erosion from './assets/pages/home/content/soil-erosion/erosion';
 
