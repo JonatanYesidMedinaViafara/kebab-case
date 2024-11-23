7a8d149b9d6c89fb9c927976c20dff59cbd19efe
Jonatan Medina <Jonatan.Medina@correounivalle.edu.co>
2024-11-23 18:11:20 -0500

    cambios en main

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
 
 import Login from './assets/pages/login/Login';
-import Home from './assets/pages/home/introduction/home';
+import home from './assets/pages/home/introduction/home';
 import Page404 from './assets/pages/page-404/Page404';
 import Deforestation from './assets/pages/home/content/deforestation/deforestation';
 import Management from './assets/pages/home/content/improper-waste-management/management';
@@ -18,8 +18,8 @@ import Erosion from './assets/pages/home/content/soil-erosion/erosion';
 const router = createBrowserRouter([
   { path: "/", element: <Navigate to="/login" replace /> },
   { path: "/login",element: <Login />,},
-  { path: "/Introduction",element: <Home />,},
-  { path: "/Map",element: <Map />,},  
+  { path: "/introduction",element: <Home />,},
+  { path: "/map",element: <Map />,},  
   { path: "/soil-erosion",element: <Erosion />,},
   { path: "/deforestation",element: <Deforestation />,},
   { path: "/management",element: <Management />,},
